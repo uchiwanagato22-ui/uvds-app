93e9812de382ec7d8abf0b83f0acc2f6c35c004e
uchiwanagato22 <uchiwanagato22@email.com>
2026-05-10 16:22:43 +0000

    fix share_plus version

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,7 +11,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:4.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
     }
 }
 
